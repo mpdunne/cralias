0ea3441317414aa63d76f1fd44acced8aa232012
Michael Dunne <mpdunne@live.com>
2018-02-23 18:11:05 +0000

    Add commented descriptions

--- cralias.sh
+++ cralias.sh
@@ -1,27 +1,40 @@
 ##########################
 # Directory navigation
 ##########################
-function setfave { pwd > ~/.faveplace ; echo "set faveplace to `pwd`"; }
-function fave { cd `cat ~/.faveplace` ;}
+# Type .. for previous directory, ... for the one before, etc.
+for i in `seq 1 30`; do al=$(for j in `seq 1 $i`; do printf '.'; done); target=$(for j in `seq 1 $i`; do printf '../'; done) ; alias .$al="cd $target"; done
+# Make directory and enter it
+function md { mkdir -p $1; cd $1 ;}
+# Home is where the heart is
+alias home='cd ~'
+# Save and recover directory history
 function cd { pwd >> ~/.dirhistory; tail -n 50 ~/.dirhistory > ~/.dirhistory.tmp; mv ~/.dirhistory.tmp  ~/.dirhistory; builtin cd $1 ;}
 function cl { if [[ -z $1 ]]; then a=1; else a=$1; fi; dest=$(tail -n $a ~/.dirhistory | head -n1); cd $dest ;}
-function cz { d=`dirname $1`; cd $d ;}
 function cdh { if [[ -z $1 ]]; then a=10; else a=$1; fi; tail -n $1 ~/.dirhistory ;}
-for i in `seq 1 30`; do al=$(for j in `seq 1 $i`; do printf '.'; done); target=$(for j in `seq 1 $i`; do printf '../'; done) ; alias .$al="cd $target"; done
-function shortcut { loc=`readlink -f $2`; echo "alias $1=\"cd $loc\"" >> ~/.bash_aliases ; source ~/.bash_aliases; }
 
-function md { mkdir -p $1; cd $1 ;}
-
-alias home='cd ~'
+###########################
+# Quick access
+###########################
 alias aliases='vim ~/.bash_aliases; source ~/.bash_aliases'
 alias bashrc="vim ~/.bashrc"
 alias sourcerc="source ~/.bashrc"
 alias pony="source ~/.bash_aliases"
-alias unik="awk '{if (a[\$0] == \"\") {print; a[\$0]=1} }'"
+
+###########################
+# Shortcuts
+###########################
+# Fave place
+function setfave { pwd > ~/.faveplace ; echo "set faveplace to `pwd`"; }
+function fave { cd `cat ~/.faveplace` ;}
+# Set a shortcut (e.g. shortcut dest ~/destination)
+function shortcut { loc=`readlink -f $2`; echo "alias $1=\"cd $loc\"" >> ~/.bash_aliases ; source ~/.bash_aliases; }
 
 ##########################
 # Sandbox
 ##########################
+# Functions for creating a sandbox environment in home directory.
+# Type sand to go to a new sandbox
+# Type clearsand to clear all sandboxes
 mkdir -p ~/.sandbox
 alias sandbox="cd ~/.sandbox"
 function sand { a=`mktemp -d ~/.sandbox/tmp.XXXXX`; cd $a ;}
@@ -38,80 +51,108 @@ alias les="less"
 ##########################
 # Text manipulation
 ##########################
+# To upper case
 function up { awk '{print toupper($0)}' ; }
+# To lower case
 function low { awk '{print tolower($0)}' ; }
+# Replace whitespace with tab
 function rt { sed -r "s/ +/\t/g" $1 ;}
-alias dehash="awk 'NR == 1 ||  \$1 ~ /^[^#]/'"
 
 ##########################
 # Finding things
 ##########################
+# Find by name
 function nfind { find . -name "$1" ;}
+# Find containing name
 function pfind { find . -name "*$1*" ;}
 
 ##########################
 # cat variants
 ##########################
+# Cat and report file names for list of regex strings
 function dog { for f in $@; do echo $f; cat $f; done;}
+# Cat all files in directory
 function puss { for file in `ls $1`; do cat $file; done ;}
+# Dog all files in directory
 function pooch { for file in `ls $1`; do dog $file; done ;}
 
 ##########################
 # Sorting things
 ##########################
+# Abbreviation of sort -k
 alias sk="sort -k"
+# Deduplicate without sorting (faster for long lists)
+alias unik="awk '{if (a[\$0] == \"\") {print; a[\$0]=1} }'"
 
 ##########################
 # less variants
 #########################
 #http://misc.flogisoft.com/bash/tip_colors_and_formatting
-function lesc { column -t -n -s $'\t' $1 | less -S ;}
-function lescn { grep -v "NA" $1 | lesc ;}
+# Less - no word wrap
 function lesss { less -S $1 ;}
+# Less - align columns, no word wrap
+function lesc { column -t -n -s $'\t' $1 | less -S ;}
+# Lesc omitting "NA"
+function lescn { grep -v "NA" $1 | lesc ;}
+# Lesss with colours
 function lesssr { less -SR $1 ;}
 
 ##########################
 # Web functions
 ##########################
+# Get all files in web directory.
 function superget { wget -r -nH -nd -np $1 -N *index* ;}
 
 ##########################
 # Processes
 ##########################
+# Watch files being changed in directory. Unsorted and sorted.
 alias vl="watch -n 0.1 ls -l"
 alias vk="watch -n 0.1 ls -lt"
+# Watch files being changed in directory - regex filtered. Unsorted and sorted.
+function vll { watch -n 0.1 "ls -l | grep -P \"$1\"" ;}
+function vkk { watch -n 0.1 "ls -lt | grep -P \"$1\"" ;}
+# Watch all files being sorted in directory and subdirectories. Sorted.
 alias fk="watch -n 0.1 'find . -printf \"%T@ %Tc %s %p \\n\" | sort -nr | column -t'"
+# Live file count for files in directory
 alias ck="watch -n 0.1 \"ls -f | wc -l \""
-function vll { watch -n 0.1 "ls -l | grep \"$1\"" ;}
-function vkk { watch -n 0.1 "ls -lt | grep \"$1\"" ;}
-function superkill { kill $(ps ux | grep "$1" | awk '{print $2}' );}
+# Careful! kill all processes issued by current user matching a regex string
+function superkill { kill $(ps ux | grep -P "$1" | awk '{print $2}' );}
 
 ##########################
 # ls variants
 ##########################
-alias lg="ls -l | grep "
+# View files matching string
+alias lg="ls -l | grep -P "
+# View hidden files, too
+alias la="ls -a"
 
 ##########################
 # grep variants
 ##########################
+# Keep colours in grep when piping to less -R or similar
 alias cgrep="\grep --color=always"
 
 ##########################
 # live word count
 ##########################
+# Watch as lines are being counted
 alias wcl="awk 'BEGIN{a=0; printf \"\\r\"a}{a++; printf \"\\r\"a}END{printf \"\\n\"}'"
 
 ##########################
 # awk variants
 ##########################
+# Awk with tab separator as default
 alias auk="awk -F'\t'"
 
 ##########################
 # calculations
 ##########################
+# Calculate the mean of a column of numbers
 alias mean="awk '{s+=\$1}END{print s/NR}' RS=\"\n\""
 
 ##########################
 # Clear screen
 ##########################
+# Clear the screen
 alias cls='printf "\033c"'
